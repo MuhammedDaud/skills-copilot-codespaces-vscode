c758e657fb4b29ad4f0ca3e5ecd63887f5a2525b
Daudiiiiiiii <163525522+MuhammedDaud@users.noreply.github.com>
2024-10-12 07:55:53 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var member = {
+        name: "John",
+        age: 30,
+        occupation: "Software Developer",
+        skills: ["JavaScript", "Node.js", "React.js"]
+    }
+    return member;
+}
